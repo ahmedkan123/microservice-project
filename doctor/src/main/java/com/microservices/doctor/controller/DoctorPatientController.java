699a5abb3cf0f8eff0172bfd55f6ec0a34dab146
Ahmed Abd-Elgawad kandel <a.kandel@smart.local>
2023-11-12 16:52:59 +0200

    finial update

--- doctor/src/main/java/com/microservices/doctor/controller/DoctorPatientController.java
+++ doctor/src/main/java/com/microservices/doctor/controller/DoctorPatientController.java
@@ -3,6 +3,7 @@ package com.microservices.doctor.controller;
 import com.microservices.doctor.model.dto.AddPatientDto;
 import com.microservices.doctor.model.dto.PatientDto;
 import com.microservices.doctor.service.DoctorPatientService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,8 @@ public class DoctorPatientController {
     public String getPatientByFeignClient(@RequestParam String name) {
         return doctorPatientService.getPatientByFeignClient(name);
     }
+
+    @CircuitBreaker(name="getPatient", fallbackMethod = "getDefaultPatient")
     @GetMapping("/get-patient-by-doctor")
     public PatientDto getPatientByFeignClient(@RequestParam Long id) {
         return doctorPatientService.getPatient(id);
@@ -26,6 +29,13 @@ public class DoctorPatientController {
     @PostMapping("/add-patient-by-doctor")
     public AddPatientDto getPatientByFeignClient(@RequestBody AddPatientDto dto) {
         return doctorPatientService.addPatient(dto);
+    }
+    public PatientDto getDefaultPatient(Exception e) {
+        return PatientDto.builder()
+                .patientName("UNKNOWN-PATIENT")
+                .id(0L)
+                .patientAge(0)
+                .build();
     }
 
 }
--- doctor/src/main/java/com/microservices/doctor/service/DoctorPatientService.java
+++ doctor/src/main/java/com/microservices/doctor/service/DoctorPatientService.java
@@ -3,11 +3,13 @@ package com.microservices.doctor.service;
 import com.microservices.doctor.model.dto.AddPatientDto;
 import com.microservices.doctor.model.dto.PatientDto;
 import com.microservices.doctor.proxy.PatientProxy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class DoctorPatientService {
     @Autowired
     private PatientProxy patientProxy;
@@ -20,7 +22,11 @@ public class DoctorPatientService {
         return patientProxy.getName(name);
     }
     public PatientDto getPatient(Long id) {
-        return patientProxy.get(id);
+        log.info("Getting patient details for id: {} " ,id);
+        PatientDto patientDto = patientProxy.get(id);
+        log.info("Patient details: {} " ,patientDto);
+        return patientDto;
+
     }
     public AddPatientDto addPatient(AddPatientDto dto) {
         return patientProxy.save(dto);
